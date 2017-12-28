bb5177173d9cc63cc7a562410ae183321bcfb6a9
SchlachterSchmidt <teenageenemanursesinbondage@gmail.com>
2017-12-28 18:35:55 +0000

    updated utils with confusion matrix plot method and load image method

--- utils.py
+++ utils.py
@@ -4,16 +4,19 @@ Module is adapted from:
 https://github.com/fastai/courses/blob/master/deeplearning1/nbs/utils.py
 """
 
-from keras.preprocessing import image
-from keras.utils.np_utils import to_categorical
+import itertools
 
 import matplotlib.pyplot as plt
+import numpy as np
+from keras.preprocessing import image
+from keras.utils.np_utils import to_categorical
+from sklearn.metrics import confusion_matrix
 
 
 def get_in_batches(dirname,
                    gen=image.ImageDataGenerator(),
                    shuffle=True,
-                   batch_size=4,
+                   batch_size=32,
                    class_mode='categorical',
                    target_size=(224, 224)):
     """
@@ -56,3 +59,49 @@ def plot_acc_and_loss(history):
     plt.xlabel('epoch')
     plt.legend(['train', 'val'], loc='upper left')
     plt.show()
+
+
+def plot_confusion_matrix(cm, classes, normalize=False,
+                          title='Confusion matrix', cmap=plt.cm.Blues):
+    """Plot confusion matrix.
+
+    This function is copied from the scikit docs:
+    http://scikit-learn.org/stable/auto_examples/model_selection/plot_confusion_matrix.html
+    """
+    plt.figure()
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+    print(cm)
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j], horizontalalignment="center",
+        color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+
+def load_image(img_path, show=False):
+    """Take image path and retun 4d image tensor.
+
+    Format: batch_size, height, width, channels
+    """
+    img = image.load_img(img_path, target_size=(224, 224))
+    img_tensor = image.img_to_array(img)
+    img_tensor = np.expand_dims(img_tensor, axis=0)
+    img_tensor /= 255.
+
+    if show:
+        plt.imshow(img_tensor[0])
+        plt.axis('off')
+        plt.show()
+
+    return img_tensor
